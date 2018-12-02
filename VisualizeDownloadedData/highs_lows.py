238e7bc30c0eb24f6d2432b452143c81c263c8ff
Maria Pantsiou <maria.th.pantsiou@gmail.com>
2018-12-02 01:24:15 +0000

    Obtaining two-digit country codes

--- VisualizeDownloadedData/highs_lows.py
+++ VisualizeDownloadedData/highs_lows.py
@@ -3,7 +3,7 @@ from matplotlib import pyplot as plt
 from datetime import datetime
 
 # Get dates, high and low temeratures from file.
-filename = "sitka_weather_2014.csv"
+filename = "death_valley_2014.csv"
 with open(filename) as f:
     reader = csv.reader(f)
 
@@ -12,24 +12,29 @@ with open(filename) as f:
     
     dates, highs, lows = [], [], []
     for row in reader:
+        try:
 
-        current_date = datetime.strptime(row[0], "%Y-%m-%d")
-        dates.append(current_date)
+            current_date = datetime.strptime(row[0], "%Y-%m-%d")
+            high = int(row[1])
+            low = int(row[3])
+        except ValueError:
+            print(current_date, "missing data")
+        
+        else:
 
-        # Convert string into intergers so the matplotlib can read them.
-        high = int(row[1])
-        highs.append(high)
-
-        low = int(row[3])
-        lows.append(low)
+            # Convert string into intergers so the matplotlib can read them.
+            dates.append(current_date)
+            highs.append(high)    
+            lows.append(low)
 
 # Plot the data.
 fig = plt.figure(dpi = 128, figsize = (10, 6))
-plt.plot(dates, highs, c = "red")
-plt.plot(dates, lows, c = "blue")
+plt.plot(dates, highs, c = "red", alpha = 0.5) # alpha for transparency
+plt.plot(dates, lows, c = "blue", alpha = 0.5)
+plt.fill_between(dates, highs, lows, facecolor = "purple", alpha = 0.3)
 
 # Format plot.
-plt.title("Daily high and low temperatures, 2014", fontsize = 24)
+plt.title("Daily high and low temperatures, Death Valley", fontsize = 24)
 plt.xlabel(" ", fontsize = 16)
 
 # Draw the dates diagonally to prevent them from overlapping
