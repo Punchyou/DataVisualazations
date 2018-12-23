5852b467d455fd9714788131d1cf8afa2401a69a
Maria Pantsiou <maria.th.pantsiou@gmail.com>
2018-12-23 00:16:33 +0000

    Refining Pygal Charts

--- Working with APIs/python_repos.py
+++ Working with APIs/python_repos.py
@@ -1,4 +1,6 @@
 import requests
+import pygal
+from pygal.style import LightColorizedStyle as LSC, LightenStyle as LS
 
 # Make an API call and store the responce.
 url = "https://api.github.com/search/repositories?q=language:python&sorted=stars"
@@ -11,11 +13,33 @@ print("Total resos:", response_dict['total_count'])
 
 # Explore information about the repositories.
 repo_dicts = response_dict['items']
-print("Repositories returned:", len(repo_dicts))
+
+names, stars = [], []
+for repo_dict in repo_dicts:
+    names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+
+# Make visualization.
+my_style = LS('#333366', base_style=LSC)
+
+my_config = pygal.Config()
+my_config.x_label_rotation = 45
+my_config.show_legend = False
+my_config.title_font_size = 24
+my_config.label_font_size = 14
+my_config.major_label_font_size = 18
+my_config.truncate_label = 15
+my_config.show_y_guides = False
+my_config.width = 1000
+
+chart = pygal.Bar(my_config, style=my_style)
+chart.title = 'Most-Starred Python Projects on GitHub'
+chart.x_labels = names
+chart.add('', stars) # we don't need this data series to be labelled, so we pass an empty string for that label.
+chart.render_to_file('C:/Users/Maria/Documents/MyCodes/DataVisualization/Working with APIs/python_repos.svg')
 
 # Examine the first respository.
 repo_dict = repo_dicts[0]
 print('\nKeys: ', len(repo_dict))
 for key in sorted(repo_dict):
     print(key)
-print("Name: ", repo_dict['name'])
